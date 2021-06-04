03e1345df8258dcb0c27dc73f29eda8870ce0246
Matteo <eversordvl@hotmail.com>
2021-06-04 17:43:39 +0200

    Added shared library, added a few paths to ignore list.

---
+++ Code/TheCheapsLib/GameSimulation.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TheCheaps
+{
+    public class GameSimulation
+    {
+        public int X;
+        public int Y;
+
+        public void Start()
+        {
+        }
+
+        public void Step()
+        {
+            var now = DateTime.Now;
+            this.X = (int)(320+320 * Math.Cos(now.Second+now.Millisecond/1000f));
+            this.Y = (int)(240 + 240 * Math.Sin(now.Second + now.Millisecond / 1000f));
+
+        }
+    }
+}
--- Code/TheCheapsServer/Program.cs
+++ Code/TheCheapsServer/Program.cs
@@ -2,21 +2,26 @@
 #define ECHO
 using Lidgren.Network;
 using System;
+using System.Net;
+using TheCheaps;
 
 namespace TheCheapsServer
 {
     class Program
     {
+        static GameSimulation simulation;
         static void Main(string[] args)
         {
             var config = new Lidgren.Network.NetPeerConfiguration("TheCheaps");
             config.Port = 12345;
             var server = new Lidgren.Network.NetServer(config);
             server.Start();
-            Console.WriteLine($"Server for {config.AppIdentifier} starting... Status is : {server.Status} on port {server.Port}");
+            Console.WriteLine($"Server for {config.AppIdentifier} starting... Listening on IP:{GetLocalIPAddress()} on port {server.Port}");
+            simulation = new GameSimulation();
+            simulation.Start();
             while (true)
             {
-                NetIncomingMessage msg = server.WaitMessage(1000);
+                NetIncomingMessage msg = server.ReadMessage();
                 if (msg == null)
                 {
 #if VERBOSE
@@ -27,38 +32,45 @@ namespace TheCheapsServer
 #if ECHO
                 Console.WriteLine($"Message received {msg}");
 #endif
+                process_message(server, msg);
+                simulation.Step();
+            }
+        }
 
-                while ((msg = server.ReadMessage()) != null)
-                {
-                    switch (msg.MessageType)
-                    {
+        private static void process_message(NetServer server, NetIncomingMessage msg)
+        {
+            switch (msg.MessageType)
+            {
 
-                        case NetIncomingMessageType.VerboseDebugMessage:
-
-                        case NetIncomingMessageType.DebugMessage:
-
-                        case NetIncomingMessageType.WarningMessage:
-
-                        case NetIncomingMessageType.ErrorMessage:
-
-                            Console.WriteLine(msg.ReadString());
-
-                            break;
-
-                        default:
-
-                            Console.WriteLine("Unhandled type: " + msg.MessageType);
-
-                            break;
-
-                    }
-
-                    server.Recycle(msg);
-
-                }
-
+                case NetIncomingMessageType.VerboseDebugMessage:
+                case NetIncomingMessageType.DebugMessage:
+                case NetIncomingMessageType.WarningMessage:
+                case NetIncomingMessageType.ErrorMessage:
+                    Console.WriteLine(msg.ReadString());
+                    break;
+                default:
+                    var text = "Unhandled type: " + msg.MessageType;
+                    Console.WriteLine(text);
+                    NetOutgoingMessage sendMsg = server.CreateMessage();
+                    sendMsg.Write(simulation.X);
+                    sendMsg.Write(simulation.Y);
+                    server.SendMessage(sendMsg, msg.SenderConnection, NetDeliveryMethod.ReliableOrdered);
+                    break;
 
             }
+            server.Recycle(msg);
+        }
+        static string GetLocalIPAddress()
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+            throw new Exception("No network adapters with an IPv4 address in the system!");
         }
     }
 }
