163e3b7ff4a3afd17717b76456d829029968902c
SabrinaVicari <sabrinavicari96@gmail.com>
2021-06-07 10:32:54 +0200

    working on entity, add inventory class

--- Code/TheCheapsLib/Entity.cs
+++ Code/TheCheapsLib/Entity.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Xna.Framework;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
@@ -6,10 +7,26 @@ using System.Text;
 namespace TheCheapsLib
 {
     //MODEL
-    //Contiene: personaggio (posizione, inventario, percorso texture, eventuale rettangolo sorgente, layer z, fisica collisione),  
+    //Contiene: posizione, inventario, percorso texture, eventuale rettangolo sorgente, layer z, fisica collisione
+
     class Entity
     {
+        private Vector2 posxy;//posizione piedi
+        private int z;
+        private string texture;
+        private Rectangle sourcerect;
+        private int direction;
+
+        private Inventory inventory;
         public Entity() { }
+        public Entity(Vector2 posxy, int z, string texture, Rectangle sourcerect/*, Inventory inventory*/) 
+        {
+            this.posxy = posxy;
+            this.z = z;
+            this.texture = texture;
+            this.sourcerect = sourcerect;
+            //this.inventory = inventory;
+        }
         public void binarywrite(BinaryWriter bw)
         {
 
