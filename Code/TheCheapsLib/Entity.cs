4032b24a19a98638e204c33995cc08233b12325b
SabrinaVicari <sabrinavicari96@gmail.com>
2021-06-07 10:10:16 +0200

    adding entity
    adding entity

---
+++ Code/TheCheapsLib/Entity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace TheCheapsLib
+{
+    //MODEL
+    //Contiene: personaggio (posizione, inventario, percorso texture, eventuale rettangolo sorgente, layer z, fisica collisione),  
+    class Entity
+    {
+        public Entity() { }
+        public void binarywrite(BinaryWriter bw)
+        {
+
+        }
+        public void binaryread(BinaryReader br)
+        {
+
+        }
+    }
+}
